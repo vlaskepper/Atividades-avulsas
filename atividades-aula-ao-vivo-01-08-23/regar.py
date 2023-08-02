962e1156248ab575bdc6f1c7b185da4cdda6fd43
vlaskepper <120275756+vlaskepper@users.noreply.github.com>
2023-08-01 21:21:57 -0300

    atividades-aula-ao-vivo-01-08-23

---
+++ atividades-aula-ao-vivo-01-08-23/regar.py
@@ -0,0 +1,9 @@
+#Vinícius Luiz e José Leite
+
+i = 0
+
+while i < 6:
+  reguei = str(i)
+  print("Reguei a planta número" ,reguei)
+  i += 1
+  
---
+++ atividades-aula-ao-vivo-01-08-23/regar3.py
@@ -0,0 +1,5 @@
+#Vinícius Luiz e Eduarda Oliveira
+
+for i in range(0, 6, 2):
+    reguei = str(i)
+    print("Reguei o tomate número" ,reguei)
