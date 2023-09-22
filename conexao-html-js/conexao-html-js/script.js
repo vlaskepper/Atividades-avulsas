1c4aaee4c3ed6090fb177a3cb0e6040046675264
Vinícius Luiz A <120275756+vlaskepper@users.noreply.github.com>
2023-09-22 10:19:32 -0300

    aula-ao-vivo-14-09-2023

---
+++ conexao-html-js/conexao-html-js/script.js
@@ -0,0 +1,38 @@
+const h3 = document.getElementById("h3");
+console.log(h3);
+
+const listaRedes = document.getElementsByClassName("lista_redes");
+console.log(listaRedes);
+
+const article = document.getElementById("post02");
+console.log(article);
+
+const sectionFormulario = document.getElementById("formulario");
+console.log(sectionFormulario);
+
+const post = document.getElementsByClassName("post");
+console.log(post);
+
+const h1 = document.querySelector("h1");
+console.log(h1);
+
+const section = document.querySelector("section");
+console.log(section);
+
+const sections = document.querySelectorAll("section");
+console.log(sections);
+
+const lista = document.querySelectorAll("#lista_redes");
+console.log(lista);
+
+const texto =  document.querySelectorAll(".post-texto");
+console.log(texto);
+
+const h2 = document.querySelectorAll("h2");
+console.log(h2);
+
+const autor = document.querySelectorAll(".post-autor");
+console.log(autor);
+
+const formulario = document.querySelectorAll("#formulario");
+console.log(formulario);
